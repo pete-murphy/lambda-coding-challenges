a0557dba179d4dd3a3fac680d577c5a6d31bd089
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-08 10:52:19 -0400

    Complete insertionSort

--- src/week13/insertionSort.js
+++ src/week13/insertionSort.js
@@ -1,1 +1,8 @@
-const insertionSort = arr => arr.reduce(() => {})
+const insert = (ys, x) => {
+  const y = ys.shift()
+  return y === undefined ? [x] : x < y ? [x, y, ...ys] : [y, ...insert(ys, x)]
+}
+
+const insertionSort = arr => arr.reduce(insert, [])
+
+insertionSort([1, 2, 3, 9, 6, 3]) //?
